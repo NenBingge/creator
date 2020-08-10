883edaf786cfe67c6fa4d50bef9e8ef37b086b63
sheng <527067594@qq.com>
2020-08-10 14:52:24 +0800

    支持数量更新

--- cacheview2/ScrollViewPro.js
+++ cacheview2/ScrollViewPro.js
@@ -1,5 +1,4 @@
 import testItem from "testItem";
-import { endianness } from "os";
 
 const EPSILON = 1e-4;
 
@@ -8,6 +7,8 @@ cc.Class({
 
     properties: {
         item_prefab: cc.Prefab,
+        item_num: 0,
+        item_height: 0,
         // item_prefabs: {
         //     type: cc.Prefab,
         //     default: [],
@@ -22,7 +23,6 @@ cc.Class({
     onLoad(){
         this.begin_index = 0;
         this.end_index = 0;
-        this.item_num = 0;
         this.item_list = [];
         this.index_arr = [];
     },
@@ -30,48 +30,119 @@ cc.Class({
     start(){
         this._super();
         var arr = [];
-        for(let i = 0 ; i < 6 ; i++){
+        for(let i = 0 ; i < 40 ; i++){
             arr.push(i);
         }
         this.initData(arr);
-       
+        
+        var self = this;
+        this.scheduleOnce(function(){
+            var arr1 = [];
+            for(let i = 0 ; i < 20 ; i++){
+                arr1.push(i);
+            }
+            self.initData(arr1);
+        }, 5);
+    },
+
+    getItem(index){
+        if(index < this.item_list.length)return this.item_list[index];
+        else{
+            if(index == this.item_list.length){
+                var node = cc.instantiate(this.item_prefab);
+                node.parent = this.content;
+                this.item_list.push(node);
+                return this.item_list[index];
+            }else{
+                console.warn("getItem index not generate")
+            }
+        }
     },
 
     initData(data){
         this.data = data;
-        var first_node = cc.instantiate(this.item_prefab);
-        first_node.parent = this.content;
-        first_node.getComponent(testItem).init(data[0] , 0);
         var begin_y = 0;
-        first_node.y = begin_y;
-        begin_y -= first_node.height;
-        var num = Math.ceil(this._view.height / first_node.height);
-        this.item_num = num + 2;
-        cc.log("item_Num:",this.item_num);
-        this.item_list.push(first_node);
-        this.index_arr.push(this.end_index);
-        var data_len = data.length;
-        for(let i = 0 ; i < this.item_num - 1 ; i++){
-            var node = cc.instantiate(this.item_prefab);
-            node.parent = this.content;
-            if(i + 1 < data_len){
-                node.getComponent(testItem).init(data[i + 1] , i + 1);
-                node.y = begin_y;
-                begin_y -= node.height;
-                this.item_list.push(node);
-                this.end_index++;
-                this.index_arr.push(this.end_index);
-            }else{
-                node.active = false;
+        var num = Math.min(data.length , this.item_num);
+        this.end_index = this.begin_index + num - 1;
+        if(this.end_index > data.length - 1){
+            var offset = this.end_index + 1 - data.length;
+            this.end_index = data.length - 1;
+            this.begin_index -= offset;
+            this.content.y = -this.begin_index * this.item_height;
+            if(this.begin_index < 0)this.begin_index = 0;
+            begin_y = -this.begin_index * this.item_height;
+        }else{
+            if(this.item_list.length > 0 && this.index_arr.length > 0){
+                begin_y = this.item_list[this.index_arr[0]].y;
             }
+        }
+        this.index_arr = [];
+        var i = 0;
+        for(; i < num ; i++){
+            var node = this.getItem(i);
+            var index = this.begin_index + i;
+            node.y = begin_y;
+            node.active = true;
+            node.getComponent(testItem).init(data[index] , index);
+            begin_y -= node.height;
+            this.index_arr.push(i);
+        }
+        for(; i < this.item_list.length ; i++){
+            this.item_list[i].active = false;
         }
         this._calculateBoundary();
     },
 
-    refreshData(data){
-        this.data = data;
+    // initData(data){
+    //     this.data = data;
+    //     var first_node = cc.instantiate(this.item_prefab);
+    //     first_node.parent = this.content;
+    //     first_node.getComponent(testItem).init(data[0] , 0);
+    //     var begin_y = 0;
+    //     first_node.y = begin_y;
+    //     begin_y -= first_node.height;
+    //     var num = Math.ceil(this._view.height / first_node.height);
+    //     if(this.item_num == 0){
+    //         this.item_num = num + 2;
+    //     }
+    //     cc.log("item_Num:",this.item_num);
+    //     this.item_list.push(first_node);
+    //     this.index_arr.push(this.end_index);
+    //     var data_len = data.length;
+    //     for(let i = 0 ; i < this.item_num - 1 ; i++){
+    //         var node = cc.instantiate(this.item_prefab);
+    //         node.parent = this.content;
+    //         if(i + 1 < data_len){
+    //             node.getComponent(testItem).init(data[i + 1] , i + 1);
+    //             node.y = begin_y;
+    //             begin_y -= node.height;
+    //             this.item_list.push(node);
+    //             this.end_index++;
+    //             this.index_arr.push(this.end_index);
+    //         }else{
+    //             node.active = false;
+    //         }
+    //     }
+    //     this._calculateBoundary();
+    // },
 
-    },
+    // refreshData(data){
+    //     if(data.length > this.data.length && this.data.length + 2 < this.item_num){
+    //         var num = data.length - this.data.length;
+    //         num = Math.min(num , this.item_num - this.data.length);
+    //         for(let i = 0 ; i < num ; i++){
+    //             var index = this.index_arr.pop();
+    //             this.item_list[index].active = false;
+    //         }
+    //     }else if(data.length < this.data.length && data.length + 2 < this.item_num){
+    //         this.index_arr = [];
+    //         for(let i = 0 ; i < data.length ; i++){
+    //             var index = this.index_arr.pop();
+    //             this.item_list[index].active = false;
+    //         }
+    //     }
+    //     this.data = data;
+    // },
 
     
 
@@ -135,9 +206,9 @@ cc.Class({
     _getContentTopBoundary () {
         if(this.index_arr.length > 0){
             if(this.begin_index == 0){
-                return this.content.y + this.item_list[0].y;
+                return this.content.y + this.item_list[this.index_arr[0]].y;
             }else{
-                return this.content.y - this.item_list[0].y;
+                return this.content.y - this.item_list[this.index_arr[0]].y;
             }
         }else{
             let contentSize = this.content.getContentSize();
